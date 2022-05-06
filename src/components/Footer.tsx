4a1d6c5fdb473b5bb556262431ec2bd594e98039
Roni Castro <roni.c.castro@gmail.com>
2022-05-06 02:15:15 -0300

    feat: create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <footer className="text-xs text-center text-neutral-400">
+      <p>
+        Feito com ♥ pela{' '}
+        <a
+          className="underline underline-offset-2"
+          target="_blank"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </p>
+    </footer>
+  );
+}
